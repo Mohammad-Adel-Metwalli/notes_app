474e81abb2fd19589f8bd9cac72ebb2f1f676f5a
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-04 01:14:38 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget
+{
+  const NotesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return const Placeholder();
+  }
+}
+
