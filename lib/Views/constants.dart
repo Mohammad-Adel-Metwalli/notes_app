7dccf0b56c6c0bc9ae3178d787c1ec5c41db7200
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-04 23:38:42 +0300

    Init hive and open notes box

--- lib/Views/constants.dart
+++ lib/Views/constants.dart
@@ -1,2 +1,3 @@
 import 'dart:ui';
 const priamryColor = Color(0xff62FCD7);
+const notesBox = 'NotesBox';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/Views/constants.dart';
 import 'package:notes_app/Views/edit_note_view.dart';
 import 'package:notes_app/Views/home_page.dart';
 
-void main()
+void main() async
 {
+  await Hive.initFlutter();
+  await Hive.openBox(notesBox);
   runApp(const NotesApp());
 }
 
