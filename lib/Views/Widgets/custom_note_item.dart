aeac6c4d0a4bcc83fe630431cbff0e3d196ba66b
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-04 21:21:58 +0300

    create edit note view

--- lib/Views/Widgets/custom_note_item.dart
+++ lib/Views/Widgets/custom_note_item.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:date_time_format/date_time_format.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'edit_note_view_body.dart';
+
 class CustomNoteItem extends StatelessWidget
 {
   CustomNoteItem({super.key, required this.title, required this.subTitle});
@@ -13,39 +15,48 @@ class CustomNoteItem extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16)
-      ),
+    return GestureDetector(
+      onTap: ()
+      {
+        Navigator.push(context, MaterialPageRoute(builder: (context)
+        {
+          return const EditNoteViewBody();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16)
+        ),
 
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 16),
-              child: Text(title!, style: const TextStyle(fontSize: 28, color: Colors.black)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Text(title!, style: const TextStyle(fontSize: 28, color: Colors.black)),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(bottom: 35),
+                child: Text(subTitle!, style: const TextStyle(fontSize: 14, color: Colors.black)),
+              ),
+              trailing: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    color: Colors.grey.withOpacity(0.1)
+                  ),
+                  child: IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.trash, color: Colors.red, size: 22,))
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(bottom: 35),
-              child: Text(subTitle!, style: const TextStyle(fontSize: 14, color: Colors.black)),
-            ),
-            trailing: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.grey.withOpacity(0.1)
-                ),
-                child: IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.trash, color: Colors.red, size: 22,))
-            ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(now.format('d M, Y'), style: const TextStyle(color: Colors.black, fontSize: 14)),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(now.format('d M, Y'), style: const TextStyle(color: Colors.black, fontSize: 14)),
+            )
+          ],
+        ),
       ),
     );
   }
