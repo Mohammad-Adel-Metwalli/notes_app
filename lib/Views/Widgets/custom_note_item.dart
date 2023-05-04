2b36c6c3343bb7cff417001896d3ac2738ae0cce
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-04 03:46:21 +0300

    Some customizing for widgets like note item(cards) and added a new folder called fonts(Poppins)

---
+++ lib/Views/Widgets/custom_note_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:date_time_format/date_time_format.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomNoteItem extends StatelessWidget
+{
+  CustomNoteItem({super.key, required this.title, required this.subTitle});
+  DateTime now = DateTime.now();
+  String? title;
+  String? subTitle;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+          color: CupertinoColors.white,
+          borderRadius: BorderRadius.circular(16)
+      ),
+
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text(title!, style: const TextStyle(fontSize: 28, color: Colors.black)),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(bottom: 35),
+              child: Text(subTitle!, style: const TextStyle(fontSize: 14, color: Colors.black)),
+            ),
+            trailing: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.grey.withOpacity(0.1)
+                ),
+                child: IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.trash, color: Colors.red, size: 22,))
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text(now.format('d M, Y'), style: const TextStyle(color: Colors.black, fontSize: 14)),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/Views/Widgets/notes_view_body.dart
+++ lib/Views/Widgets/notes_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:notes_app/Views/Widgets/custom_note_item.dart';
 
 import 'custom_app_bar.dart';
 
@@ -12,10 +13,14 @@ class NotesViewBody extends StatelessWidget
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
-        children: const [
-          SizedBox(height: 50,),
+        children: [
+          const SizedBox(height: 50,),
 
-          CustomAppBar(),
+          const CustomAppBar(),
+
+          const SizedBox(height: 35,),
+
+          CustomNoteItem(title: 'Flutter Tips', subTitle: 'Build Your Career With Tharwat Samy')
         ],
       ),
     );
