7dfc2bea527104d0dc3bee51e2fda5de491cae16
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-06 01:16:08 +0300

    Colors list view part 2

--- lib/Views/Widgets/color_item.dart
+++ lib/Views/Widgets/color_item.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget
 {
-  const ColorItem({Key? key}) : super(key: key);
+  const ColorItem({Key? key, required this.isSelected, required this.color}) : super(key: key);
+  final bool isSelected;
+  final Color color;
 
   @override
   Widget build(BuildContext context)
   {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
+    return isSelected ? CircleAvatar(
+      radius: 38,
+      backgroundColor: Colors.white,
+      child: CircleAvatar(
+        backgroundColor: color,
+        radius: 34,
+      ),
+    ) : CircleAvatar(
+      backgroundColor: color,
       radius: 38,
     );
   }
--- lib/Views/Widgets/colors_list_view.dart
+++ lib/Views/Widgets/colors_list_view.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:notes_app/Views/Widgets/color_item.dart';
 
-class ColorsListView extends StatelessWidget
+class ColorsListView extends StatefulWidget
 {
   const ColorsListView({Key? key}) : super(key: key);
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView>
+{
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xffAC3931),
+    Color(0xffE5D352),
+    Color(0xffD9E76C),
+    Color(0xff537D8D),
+    Color(0xff482C3D),
+  ];
 
   @override
   Widget build(BuildContext context)
@@ -11,11 +26,26 @@ class ColorsListView extends StatelessWidget
     return SizedBox(
       height: 38*2,
       child: ListView.builder(
-          itemCount: 5,
+          itemCount: colors.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index)
       {
-        return const ColorItem();
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 6),
+          child: GestureDetector(
+            onTap: ()
+            {
+              currentIndex = index;
+              setState(() {
+
+              });
+            },
+            child: ColorItem(
+              color: colors[index],
+              isSelected: currentIndex == index,
+            ),
+          ),
+        );
       }),
     );
   }
