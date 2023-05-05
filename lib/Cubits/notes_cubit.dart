ba351dea6a51209525dc2afe1d80cf62decb2206
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-05 21:40:44 +0300

    Create fetch notes method

--- lib/Cubits/notes_cubit.dart
+++ lib/Cubits/notes_cubit.dart
@@ -1,10 +1,27 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
 import '../Views/Models/note_model.dart';
+import '../Views/constants.dart';
 
 part 'notes_state.dart';
 
-class NotesCubit extends Cubit<NotesState> {
+class NotesCubit extends Cubit<NotesState>
+{
   NotesCubit() : super(NotesInitial());
+
+  fetchAllNotes() async
+  {
+    try
+    {
+      var boxNotes = Hive.box<NoteModel>(notesBox);
+      emit(NotesSuccess(boxNotes.values.toList()));
+    }
+
+    on Exception catch (e)
+    {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/Views/notes_view.dart
+++ lib/Views/notes_view.dart
@@ -1,33 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/Cubits/notes_cubit.dart';
 import 'package:notes_app/Views/Widgets/add_note_bottom_sheet.dart';
 import 'Widgets/notes_view_body.dart';
 
-class NotesView extends StatelessWidget
-{
+class NotesView extends StatelessWidget {
   const NotesView({Key? key}) : super(key: key);
   static String id = 'NotesView';
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Scaffold(
-      body: const NotesViewBody(),
-      floatingActionButton: FloatingActionButton(
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        body: const NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.white,
-          onPressed: ()
-          {
+          onPressed: () {
             showModalBottomSheet(
                 isScrollControlled: true,
                 shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(30))
+                    borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(30))
                 ),
-                context: context, 
-                builder: (context)
-            {
-              return const AddNoteBottomSheet();
-            });
+                context: context,
+                builder: (context) {
+                  return const AddNoteBottomSheet();
+                });
           },
           child: const Icon(Icons.note_add, color: Colors.green),
+        ),
       ),
     );
   }
